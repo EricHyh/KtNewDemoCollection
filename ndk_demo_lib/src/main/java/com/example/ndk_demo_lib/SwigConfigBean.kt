b0d94760779024d7f72ee82a5bb3f8f1278bfc52
eriche <eriche@futunn.com>
2025-01-03 19:40:52 +0800

    ...

--- ndk_demo_lib/src/main/java/com/example/ndk_demo_lib/SwigConfigBean.kt
+++ ndk_demo_lib/src/main/java/com/example/ndk_demo_lib/SwigConfigBean.kt
@@ -1,5 +1,6 @@
 package com.example.ndk_demo_lib
 
+
 /**
  * TODO: Add Description
  *
@@ -7,35 +8,57 @@ package com.example.ndk_demo_lib
  */
 
 
-
-
 data class ModuleConfig(
     val module_name: String,
-    val package_name: String,
-    val imports: List<String>,
+    val path_dir: String,
+    val classes: List<ClassConfig>,
 )
 
+data class TypeConfig(
+    val type: Int,
+    val class_config: ClassConfig?,
+    val enumclass_config: EnumClassConfig?,
+    val namespace_config: NamespaceConfig?,
+    val function_config: FunctionConfig?,
+)
+
+
+// ClassConfig -> BuildInfo()
+// source_path -> List<ClassConfig>
+
 data class ClassConfig(
-    val original_name: String,
-    val rename: String,
-    val package_name: String,
-    val imports: List<String>,
+    val source_path: String,
+    val original_type: String,
+    val target_type: String,
+    val is_template: Boolean,
+    val shared_ptr: Boolean,
+    val detector: Boolean,
+
+    val dependent_modules: List<String>,
+)
+
+data class EnumClassConfig(
+    val source_path: String,
+    val original_type: String,
+    val target_type: String,
+)
+
+data class NamespaceConfig(
+    val source_path: String,
+    val original_type: String,
+    val target_type: String,
+
+    val dependent_modules: List<String>
 )
 
 data class FunctionConfig(
-    val original_name: String,
-    val rename: String,
-    val package_name: String,
-    val imports: List<String>,
+    val source_path: String,
+    val original_type: String,
+    val target_type: String,
+    val is_template: Boolean,
 
-    val template_name: String,
-    val params: List<String>,
-)
+    //val template_name: String,
+    val params: Pair<String, String>,
 
-
-data class NameSpaceConfig(
-    val original_name: String,
-    val rename: String,
-    val package_name: String,
-    val imports: List<String>,
+    val dependent_modules: List<String>
 )
